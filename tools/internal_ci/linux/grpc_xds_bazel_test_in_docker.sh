9d6618d53239d730203386855f8118f842837902
Eshita Chandwani <emchandwani@google.com>
2026-05-08 14:28:35 +0530

    change directory

--- tools/internal_ci/linux/grpc_xds_bazel_test_in_docker.sh
+++ tools/internal_ci/linux/grpc_xds_bazel_test_in_docker.sh
@@ -92,3 +92,6 @@ GRPC_VERBOSITY=debug GRPC_TRACE=xds_client,xds_resolver,xds_cluster_manager_lb,c
     --verbose \
     ${XDS_V3_OPT-} \
     --client_cmd='bazel-bin/test/cpp/interop/xds_interop_client --server=xds:///{server_uri} --stats_port={stats_port} --qps={qps} {fail_on_failed_rpc} {rpcs_to_send} {metadata_to_send}'
+
+# Ensure logs are copied back to the mounted workspace for Kokoro artifact collection
+cp -rf /var/local/git/grpc/reports /var/local/jenkins/grpc/
